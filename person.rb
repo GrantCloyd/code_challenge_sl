ba464aab1f4ef1a0185122fe80ce38a53ad798fa
Grant Cloyd <dgcloyd@gmail.com>
2021-10-12 15:26:47 -0500

    clean up class and person_spec syntax

--- person.rb
+++ person.rb
@@ -17,16 +17,13 @@ class Person
     @color = color
   end 
   
-  def stringify_instance
-    "#{@last_name} #{@first_name} #{@gender} #{@date_of_birth} #{@color}"
-  end
-  
+
   def self.create_with_string(str_params)
     if str_params.class != String then return "Please provide a string" end  
     data = self.sanitize(str_params)
     if data == "Error" then return "Please provide the proper # of arguments" end   
          
-            #in order: last_name, first_name, gender, date_of_birth, color
+         #in order: last_name, first_name, gender, date_of_birth, color
     person = Person.new(data[0], data[1], data[2], data[3], data[4]) 
     self.all << person
     person
@@ -47,11 +44,15 @@ class Person
     self.output(data)
   end
 
+  def stringify_instance
+    "#{@last_name} #{@first_name} #{@gender} #{@date_of_birth} #{@color}"
+  end
+  
 
   private
 
   def self.sanitize(str_params)  
-            # replace all '| ' and ',' with an empty string 
+            # replace all '| ' and ','
     data_arr = str_params.gsub(/\|\s|,/, "").split(" ")
     #remove middle initial as needed
     data_arr.slice!(2) if data_arr.length == 6
@@ -66,35 +67,10 @@ class Person
   end
 
   def self.output(data_arr)
-    data_arr.collect {|person| p person.stringify_instance }.join("\n")
+    data_arr.collect {|person| person.stringify_instance }.join("\n")
   end
 
+  
 
 end
 
-
-
-# ---
-# #Space
-# Person.create_with_string("Kournikova Anna F F 6-3-1975 Red")
-#     Person.create_with_string("Hingis Martina M F 4-2-1979 Green")
-#         Person.create_with_string("Seles Monica H F 12-2-1973 Black")
-# #Comma
-# Person.create_with_string("Abercrombie, Neil, Male, Tan, 2/13/1943")
-#     Person.create_with_string("Bishop, Timothy, Male, Yellow, 4/23/1967")
-#         Person.create_with_string("Kelly, Sue, Female, Pink, 7/12/1959")
-# #Pipe
-# Person.create_with_string("Smith | Steve | D | M | Red | 3-3-1985")
-#     Person.create_with_string("Bonk | Radek | S | M | Green | 6-3-1975")
-#         Person.create_with_string("Bouillon | Francis | G | M | Blue | 6-3-1975")
-
-
-# print "\n"
-#  p "Output 1" 
-# p Person.sort_by_gender_and_last_name
-# print "\n"
-# p "Output 2"
-# Person.sort_by_date_of_birth_and_last_name
-# print "\n"
-# p "Output 3"
-# Person.sort_by_last_name_desc
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,19 +1,16 @@
 
  #Space
-#Person.create_with_string("Kournikova Anna F F 6-3-1975 Red")
     Person.create_with_string("Hingis Martina M F 4-2-1979 Green")
         Person.create_with_string("Seles Monica H F 12-2-1973 Black")
 #Comma
-#Person.create_with_string("Abercrombie, Neil, Male, Tan, 2/13/1943")
     Person.create_with_string("Bishop, Timothy, Male, Yellow, 4/23/1967")
         Person.create_with_string("Kelly, Sue, Female, Pink, 7/12/1959")
 #Pipe
-# Person.create_with_string("Smith | Steve | D | M | Red | 3-3-1985")
     Person.create_with_string("Bonk | Radek | S | M | Green | 6-3-1975")
         Person.create_with_string("Bouillon | Francis | G | M | Blue | 6-3-1975")
 
 describe 'Person #create_with_string:' do
-    it 'Takes a string that is formatted to be either in the space, comma, or pipe style' do
+    it 'Takes a string that is formatted in the space, comma, or pipe style.' do
         person1 = Person.create_with_string("Kournikova Anna F F 6-3-1975 Red")
         person2 = Person.create_with_string("Abercrombie, Neil, Male, Tan, 2/13/1943")
         person3 = Person.create_with_string("Smith | Steve | D | M | Red | 3-3-1985")
@@ -22,8 +19,8 @@ describe 'Person #create_with_string:' do
     end
 end
 
-describe 'Person #create_with_string:' do
-    it 'Handles formatting errors if parameters are not a string or if the wrong number of parameters are passed in the parsed string' do
+describe 'Person #create_with_string error handling:' do
+    it 'Handles formatting errors if parameters are not a string or if the wrong number of parameters are passed in the parsed string.' do
         error1 = Person.create_with_string({name: "Bob"})
         error2 = Person.create_with_string("Smith Steve Sad Lion ")   
         expect(error1).to eq("Please provide a string")
@@ -31,8 +28,9 @@ describe 'Person #create_with_string:' do
     end
 end
 
+
 describe "Person #sort_by_gender_and_last_name: Order #1" do
-    it 'Sorts all instances by gender and last name ascending' do
+    it 'Sorts all instances by gender and last name ascending.' do
 
          expect(Person.sort_by_gender_and_last_name).to eq( ["Hingis Martina Female 4/2/1979 Green",
             "Kelly Sue Female 7/12/1959 Pink",
@@ -48,7 +46,7 @@ describe "Person #sort_by_gender_and_last_name: Order #1" do
 end 
 
 describe "Person #sort_by_date_of_birth_and_last_name: Order #2" do
-    it 'Sorts all instances by DOB and last name ascending' do
+    it 'Sorts all instances by DOB and last name ascending.' do
          expect(Person.sort_by_date_of_birth_and_last_name).to eq( ["Abercrombie Neil Male 2/13/1943 Tan",
             "Kelly Sue Female 7/12/1959 Pink",
             "Bishop Timothy Male 4/23/1967 Yellow",
@@ -63,7 +61,7 @@ describe "Person #sort_by_date_of_birth_and_last_name: Order #2" do
 end 
 
 describe "Person #sort_by_last_name_desc: Order #3" do
-    it 'Sorts all instances by last name descending' do
+    it 'Sorts all instances by last name descending.' do
          expect(Person.sort_by_last_name_desc).to eq( ["Smith Steve Male 3/3/1985 Red",
             "Seles Monica Female 12/2/1973 Black",
             "Kournikova Anna Female 6/3/1975 Red",
@@ -77,3 +75,17 @@ describe "Person #sort_by_last_name_desc: Order #3" do
 
 end 
 
+def display_data
+  data_arr = [Person.sort_by_gender_and_last_name, Person.sort_by_date_of_birth_and_last_name, Person.sort_by_last_name_desc]
+
+  print "--- Formatted Data ---\n"
+
+  data_arr.each.with_index(1) do |sort_method_data, count|
+    print "\nOutput #{count}\n"
+    puts sort_method_data
+  end
+
+  print "\n---Tests---\n"
+end
+
+display_data()
